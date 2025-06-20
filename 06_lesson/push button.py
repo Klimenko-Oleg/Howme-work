9d32f94ef3693e31581bf2a76a201fb868582094
Oleg <jarlax89@gmail.com>
2025-06-20 14:31:10 +0300

    добавлены отступы

--- 06_lesson/push button.py
+++ 06_lesson/push button.py
@@ -5,6 +5,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
+
 serv = ChromeService(ChromeDriverManager().install())
 driver = webdriver.Chrome(service=serv)
 driver.maximize_window()
@@ -20,5 +21,6 @@ ajax_button.click()
 green_label = WebDriverWait(driver, 17).until(
     EC.presence_of_element_located((By.CSS_SELECTOR, "p.bg-success"))
 )
+
 text = green_label.text
 print(text)
